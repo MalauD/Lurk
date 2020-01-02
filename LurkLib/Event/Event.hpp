6ea8026341f9c6c4f853be67a76d4b6c0d14949b
Malaury <malaurydutour@outlook.fr>
2020-01-02 18:56:20 +0100

    Unit test setup

--- LurkLib/Event/Event.hpp
+++ LurkLib/Event/Event.hpp
@@ -13,16 +13,25 @@ namespace Lurk {
 		class Event
 		{
 		public:
+			Event<EventType, ArgType>();
+
 			void Call(EventArgs<EventType, ArgType>);
 
-			Event<EventType, ArgType>& operator+=(const EventFunc&);
-			void Subscribe(const EventFunc&);
+			Event<EventType, ArgType>& operator+=(const EventFunc<EventType, ArgType>);
+			void Subscribe(const EventFunc<EventType, ArgType>);
 
 			void UnSubscribe();
+
+			EventFunc<EventType, ArgType> GetFunc();
 		private:
-			EventFunc& subscribedFunc;
-			void AddSubscriber(EventFunc& func);
+			EventFunc<EventType, ArgType> subscribedFunc;
+			void AddSubscriber(const EventFunc<EventType, ArgType> func);
 		};
+
+		template<typename EventType, typename ArgType>
+		Event<EventType, ArgType>::Event()
+		{
+		}
 
 		template<typename EventType, typename ArgType>
 		inline void Event<EventType, ArgType>::Call(EventArgs<EventType, ArgType> args)
@@ -31,13 +40,13 @@ namespace Lurk {
 		}
 
 		template<typename EventType, typename ArgType>
-		inline Event<EventType, ArgType>& Event<EventType, ArgType>::operator+=(const EventFunc<EventType, ArgType>& func)
+		inline Event<EventType, ArgType>& Event<EventType, ArgType>::operator+=(const EventFunc<EventType, ArgType> func)
 		{
 			AddSubscriber(func);
 		}
 
 		template<typename EventType, typename ArgType>
-		inline void Event<EventType, ArgType>::Subscribe(const EventFunc<EventType, ArgType>& func)
+		inline void Event<EventType, ArgType>::Subscribe(const EventFunc<EventType, ArgType> func)
 		{
 			AddSubscriber(func);
 		}
@@ -49,7 +58,13 @@ namespace Lurk {
 		}
 
 		template<typename EventType, typename ArgType>
-		inline void Event<EventType, ArgType>::AddSubscriber(EventFunc<EventType, ArgType>& func)
+		inline EventFunc<EventType, ArgType> Event<EventType, ArgType>::GetFunc()
+		{
+			return subscribedFunc;
+		}
+
+		template<typename EventType, typename ArgType>
+		inline void Event<EventType, ArgType>::AddSubscriber(const EventFunc<EventType, ArgType> func)
 		{
 			subscribedFunc = func;
 		}
---
+++ LurkLibTest/Test/Event/Event_test.cpp
@@ -0,0 +1,52 @@
+#pragma once
+#include "gtest/gtest.h"
+#include "./../../../LurkLib/Event/Event.hpp"
+#include "./../../../LurkLib/Event/ServerEvents/ServerEventTypes.hpp"
+#include "./../../../LurkLib/Event/ServerEvents/ServerEventArgs.hpp"
+
+using namespace Lurk::Event;
+
+
+TEST(Subscribe_test) {
+	Event<ServerEventTypes, int> myEvent;
+
+	ASSERT_FALSE((bool)myEvent.GetFunc());
+
+	myEvent.Subscribe([&](EventArgs<ServerEventTypes, int> args) {
+			ASSERT_TRUE(false);
+		});
+
+	ASSERT_TRUE((bool)myEvent.GetFunc());
+
+			
+}
+
+TEST(EventTest,UnSubscribe_test) {
+	Event<ServerEventTypes, int> myEvent;
+
+	myEvent.Subscribe([&](EventArgs<ServerEventTypes, int> args) {
+			ASSERT_TRUE(false);
+		});
+
+	myEvent.UnSubscribe();
+
+	ASSERT_FALSE((bool)myEvent.GetFunc());
+}
+
+TEST(EventTest,GetFunc_test) {
+	ASSERT_TRUE(true);
+}
+
+TEST(EventTest,CallTest) {
+	Event<ServerEventTypes, int> myEvent;
+
+	myEvent.Subscribe([&](EventArgs<ServerEventTypes, int> args) {
+			ASSERT_EQ(args.GetArg(), 20);
+			ASSERT_EQ((int)args.GetType(), 0);
+		});
+
+	auto args = ServerEventArgs<int>(ServerEventTypes::OnNewClient,20);
+
+	myEvent.Call(args);
+}
+

bcd265294f9d9de3bb8ba4b54ae349078387c8aa
Malaury <malaurydutour@outlook.fr>
2020-01-28 19:09:57 +0100

    Setup of flatbuffers

--- LurkCoreLib/Test/Node/ClientHandler_test.hpp
+++ LurkCoreLib/Test/Node/ClientHandler_test.hpp
@@ -4,6 +4,7 @@
 #include "./../Network/SocketMock.hpp"
 #include "./../../Src/Network/TcpClient.hpp"
 #include "./../../Src/Node/ClientHandler.hpp"
+#include "./../../Src/Schemas/Version_generated.h"
 
 using namespace Lurk::Node;
 using ::testing::_;
@@ -12,6 +13,4 @@ using ::testing::Return;
 TEST(ClientHandlerTest, Ctor_test) {
 	MockSocket* mock = new MockSocket();
 	ClientHandler handler(Lurk::Network::TcpClient(std::unique_ptr<Socket>(mock)));
-
-	EXPECT_
 }
